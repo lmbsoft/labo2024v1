73afb9d2350af232f1ec40b37d20517b6a407d53
lmbsoft <lmbsoft@yahoo.com>
2024-05-11 12:57:51 -0300

    agregado intento num_iterations ascendente exp06

--- src/workflow-01/509_workflow_semillerio_sinbo_zz_loop.r
+++ src/workflow-01/509_workflow_semillerio_sinbo_zz_loop.r
@@ -354,7 +354,7 @@ corrida_baseline_semillerio_202109 <- function( pnombrewf, pvirgen=FALSE )
   # Iterar sobre las configuraciones y llamar a ZZ_final_semillerio_baseline
   for (i in 1:nrow(configuraciones_suboptimas)) {
     # Llamar a ZZ_final_semillerio_baseline con la configuración subóptima
-    nombre_experimento <- paste0("ZZ0001-sem-fijo-05-", i)
+    nombre_experimento <- paste0("ZZ0001-sem-fijo-06-", i)
     ZZ_final_semillerio_baseline(nombre_experimento,
      c("HT0001-sem-01-zz","TS0001-sem"),
      hiperparametros = configuraciones_suboptimas[i, ],
@@ -399,7 +399,7 @@ corrida_baseline_semillerio_202107 <- function( pnombrewf, pvirgen=FALSE )
   # Iterar sobre las configuraciones y llamar a ZZ_final_semillerio_baseline
   for (i in 1:nrow(configuraciones_suboptimas)) {
     # Llamar a ZZ_final_semillerio_baseline con la configuración subóptima
-    nombre_experimento <- paste0("ZZ0002-sem-fijo-05-", i)
+    nombre_experimento <- paste0("ZZ0002-sem-fijo-06-", i)
     ZZ_final_semillerio_baseline(nombre_experimento,
      c("HT0002-sem-02-zz","TS0002-sem"),
      hiperparametros = configuraciones_suboptimas[i, ],
@@ -415,12 +415,12 @@ corrida_baseline_semillerio_202107 <- function( pnombrewf, pvirgen=FALSE )
 #Aqui empieza el programa
 
 
-corrida_baseline_semillerio_202109( "basem01-zz-05" )
+corrida_baseline_semillerio_202109( "basem01-zz-06" )
 
 
 # Luego partiendo de  FE0001
 # genero TS0002, HT0002 y ZZ0002
 
-corrida_baseline_semillerio_202107( "basem02-zz-05" )
+corrida_baseline_semillerio_202107( "basem02-zz-06" )
 
  
