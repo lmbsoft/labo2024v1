bf320603587164b99abccf3d1ddf9588e5f5d002
lmbsoft <lmbsoft@yahoo.com>
2024-05-12 02:43:39 -0300

    ajuste final sobre workflow semillerío entregado

--- src/workflow-01/509_workflow_semillerio.r
+++ src/workflow-01/509_workflow_semillerio.r
@@ -375,6 +375,6 @@ corrida_baseline_semillerio_202109( "basem01" )
 # Luego partiendo de  FE0001
 # genero TS0002, HT0002 y ZZ0002
 
-corrida_baseline_semillerio_202107( "basem02" )
+#corrida_baseline_semillerio_202107( "basem02" )
 
  
